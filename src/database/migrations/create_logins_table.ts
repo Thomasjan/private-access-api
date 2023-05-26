233ce355fa5f7f753bbf8f112a7459a985298fe6
ThomasJanko <thomas78125@gmail.com>
2023-05-26 10:14:42 +0200

    Update Longins / delet Entreprise

--- src/database/migrations/create_logins_table.ts
+++ src/database/migrations/create_logins_table.ts
@@ -4,10 +4,15 @@ export const up = (knex: Knex): Knex.SchemaBuilder => {
   return knex.schema.createTable('logins', (table) => {
     table.increments('id').primary();
     table.string('date').notNullable();
-    table.integer('user_id').unsigned();
-    table.foreign('user_id').references('users.id');
-    table.integer('entreprise_id').unsigned();
-    table.foreign('entreprise_id').references('entreprises.id');
+    table.string('social_reason').notNullable();
+    table.string('name').notNullable();
+    table.string('surname').notNullable();
+    table.string('category').notNullable();
+    table.string('subcategory').notNullable();
+    // table.integer('user_id').unsigned();
+    // table.foreign('user_id').references('users.id');
+    // table.integer('entreprise_id').unsigned();
+    // table.foreign('entreprise_id').references('entreprises.id');
     table.timestamps(true, true);
   });
 };
