0cb347fe45c11e5055ab739a5e36c29dfaf82fd2
Thomas Jankowski <thomas.jankowski@gestimum.com>
2023-06-07 17:44:40 +0200

    connection database gestimum sql-srv

--- src/database.ts
+++ src/database.ts
@@ -1,22 +1,52 @@
 import mysql from 'mysql2';
 require('dotenv').config();
+import * as sql from 'mssql';
 
 
+  //Connextion Mysql private_access
+  const connection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  });
 
-const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-});
 
-// console.log(connection)
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the database!');
-});
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
+    }
+    console.log('Connected to the database!');
+  });
 
-export default connection;
+
+  //Connextion SQL Server Gestimum
+  const dbHost = process.env.DB_HOST_SQLSRV || 'localhost';
+  const dbPort = process.env.DB_PORT_SQLSRV ? parseInt(process.env.DB_PORT_SQLSRV) : 1433;
+  const dbConfig: sql.config = {
+    server: dbHost,
+    port: dbPort,
+    user: process.env.DB_USERNAME_SQLSRV || '',
+    password: process.env.DB_PASSWORD_SQLSRV || '',
+    database: process.env.DB_DATABASE_SQLSRV || '',
+    options: {
+      encrypt: false, // Use this option if your SQL Server requires an encrypted connection
+      trustServerCertificate: true,
+    },
+  };
+
+const connectionGestimum = new sql.ConnectionPool(dbConfig);
+
+const executeQuery = async (query: string): Promise<any> => {
+
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool.request().query(query);
+    return result.recordset;
+  } catch (error) {
+    throw new Error(`Error executing query: ${error}`);
+  } 
+}
+
+export { connection, connectionGestimum, executeQuery };
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import path from 'path';
 
+import axios from 'axios';
+
 
 require('dotenv').config();
 import apiRouter from './routes';
+import { connection, connectionGestimum, executeQuery} from './database';
 
-
+// const linkedin_access_token = 'AQVJyaUPuB4hHVHq2aug1o7ivONkRQb3-6Y7JrKLY40UrRQ-UIp3hLyKVJ2Ax1vLpfuxnGzIMhBknlUzeJUG1q3iyQY3xcktI-qzvqQlWvQQoWoApbMDUUIrLA16ZMQfdhOZaYMZOv92CU0krjoQ7UtfQjiusnZo0LcJ94bMRwPJlxnmz612gH6AEQmS-4pk7dzY539N3W28C1PUSHNE1njOuP7qdyJGFvCcmzGkbf0xn0JnbpkADXdiKEfa_q8VrgrcvRPHFy9lDniben2XVmhYlV9_wGE2dH2m8EVRJy-7KsY8fWAC-WxFl4j4h_83nBcpB2fFXAmlCxi1GhBG9gMphnWrRQ'
 
 const app = express()
 const port = 3000
@@ -27,4 +30,56 @@ app.use(ulpoadUrl, express.static(uploadsPath));
 app.use('/api', apiRouter);
 
 
+
+app.get('/test', (req: Request, res: Response) => {
+    connection.query('SELECT * FROM Entreprises', (err, results: any) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send({ message: 'Server Error' });
+        return;
+      }
+  
+      res.status(200).json(results);
+    });
+  });
+
+ 
+  
+  // Define your route handler
+  app.get('/testGestimum', async (req: Request, res: Response) => {
+    try {
+        const query = 'SELECT PCF_CODE, PCF_RS, PCF_EMAIL, PCF_RUE, PCF_CP, PCF_VILLE, PAY_CODE, PCF_TYPE FROM TIERS ORDER BY PCF_RS, PCF_TYPE OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY';
+      const tiers = await executeQuery(query);
+      res.status(200).json(tiers);
+    } catch (error) {
+      console.error('Error executing query:', error);
+      res.status(500).send({ message: 'Server Error' });
+    }
+  });
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+
+
+
+
+// app.get('/posts', async (req: Request, res: Response) => {
+//     try {
+//       const token = linkedin_access_token; // Replace with your LinkedIn access token
+//       const apiUrl = 'https://api.linkedin.com/v2/me';
+//       const response = await axios.get(`${apiUrl}`, {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       });
+//       const posts = response.data.elements;
+//       res.json(posts);
+//     } catch (error) {
+//       console.error('Error retrieving LinkedIn posts:', error);
+//       res.status(500).json({ fail: 'Failed to retrieve LinkedIn posts', error });
+//     }
+//   });
