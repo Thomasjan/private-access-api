a8bfad0d2762a47d7531e43d7907db77f52ed87d
Thomas Jankowski <thomas.jankowski@gestimum.com>
2023-06-12 12:14:06 +0200

    delete Pdf

--- src/routes/pdfs.route.ts
+++ src/routes/pdfs.route.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 
 const router = Router();
 
-import { getSupportsPdfs, editSupportPdf, addSupportPdf } from '../controllers/pdfs.controller';
+import { getSupportsPdfs, editSupportPdf, addSupportPdf, deleteSupportPdf } from '../controllers/pdfs.controller';
 
 router.get('/getSupportsPdfs', getSupportsPdfs);
 router.put('/editSupportPdf/:id', editSupportPdf);
 router.post('/addSupportPdf', addSupportPdf);
+router.delete('/deleteSupportPdf/:id', deleteSupportPdf);
 
 
 export default router;
