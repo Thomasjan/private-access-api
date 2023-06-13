0ae6148f4ea8ccabf2997a19b0f468e98cea4786
Thomas Jankowski <thomas.jankowski@gestimum.com>
2023-06-13 14:55:27 +0200

    PDFS table

---
+++ src/database/migrations/create_pdfs_table.ts
@@ -0,0 +1,25 @@
+import { Knex } from 'knex';
+
+export const up = async (knex: Knex): Promise<void> => {
+  await knex.schema.createTable('pdfs', (table) => {
+    table.increments('id').primary();
+    table.string('ref').notNullable();
+    table.string('title').notNullable();
+    table.string('category').nullable();
+    table.string('link').notNullable();
+    table.timestamps(true, true);
+  });
+
+  await knex('pdfs').insert([
+    { ref: 'support', title: 'Préconisations techniques et installation Gestimum ERP', category: 'Général', link: 'http://docs.gestimum.com/ERP/8/Installation/#t=Pages%2FPreambule.htm' },
+    { ref: 'support', title: 'Installation Gestimum ERP - Mise à jour de version', category: 'Général', link: 'http://docs.gestimum.com/ERP/8/Installation/#t=Pages%2FPreambule.htm' },
+    { ref: 'support', title: "Création d'un champ personnalisé", category: "l'intégré", link: 'http://docs.gestimum.com/ERP/8/Installation/#t=Pages%2FPreambule.htm' },
+    { ref: 'support', title: "Création d'un champ person 	Création Champs personnalisés avec formulesnalisé", category: "l'intégré", link: 'http://docs.gestimum.com/ERP/8/Installation/#t=Pages%2FPreambule.htm' },
+    
+  ]);
+};
+
+export const down = (knex: Knex): Knex.SchemaBuilder => {
+  return knex.schema.dropTable('pdfs');
+};
+
--- src/routes/pdfs.route.ts
+++ src/routes/pdfs.route.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 const router = Router();
 
-import { getSupportsPdfs, editSupportPdf, addSupportPdf, deleteSupportPdf } from '../controllers/pdfs.controller';
+import { getSupportsPdfs, editPdf, addPdf, deletePdf, getAideVentePdfs } from '../controllers/pdfs.controller';
 
 router.get('/getSupportsPdfs', getSupportsPdfs);
-router.put('/editSupportPdf/:id', editSupportPdf);
-router.post('/addSupportPdf', addSupportPdf);
-router.delete('/deleteSupportPdf/:id', deleteSupportPdf);
+router.get('/getAideVentePdfs', getAideVentePdfs);
+
+router.put('/editPdf/:id', editPdf);
+router.post('/addPdf', addPdf);
+router.delete('/deletePdf/:id', deletePdf);
+
+
 
 
 export default router;
