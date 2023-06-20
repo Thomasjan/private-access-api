f26de91529a04ea3d226779307b6bd6fa44f09ec
Thomas Jankowski <thomas.jankowski@gestimum.com>
2023-06-20 14:23:28 +0200

    Retrieve Posts with media Url

--- src/controllers/linkedin.controller.ts
+++ src/controllers/linkedin.controller.ts
@@ -7,7 +7,7 @@ import axios from 'axios';
   export const getLinkedinPosts = async (req: Request, res: Response) => {
     const accessToken = process.env.LINKEDIN_ACCESS_TOKEN;
     const page = 1; // Page de départ
-    const count = 100; // Nombre de résultats par page
+    const count = 100; // Nombre de résultats par page (MAX 100)
 
 
     const config = {
@@ -19,18 +19,53 @@ import axios from 'axios';
     },
     };
 
-    axios.get(`https://api.linkedin.com/v2/posts?author=urn%3Ali%3Aorganization%3A1201305&q=author&count=${count}`, config)
-    .then(response => {
-        // Gérer les publications récupérées dans la réponse
-        const posts = response.data.elements;
-        res.status(200).json(posts)
-        console.log(colors.bold.cyan(`getLinkedinPosts !`));
-      })
-    .catch(error => {
-        // Gérer l'erreur
-        res.status(500).json(error)
-        console.error(error);
-    });
+    // Récupérer les publications de Linkedin
+    try {
+      const response = await axios.get(`https://api.linkedin.com/v2/posts?author=urn%3Ali%3Aorganization%3A1201305&q=author&count=${count}`, config);
+      const posts = response.data.elements;
+      console.log(colors.bold.cyan(`getLinkedinPosts!`));
+  
+      const postsWithImage = await Promise.all(posts.map(async (post: any) => {
+        if (post.content?.article?.thumbnail) {
+          const thumbnail = post.content.article.thumbnail;
+          const imageResponse = await axios.get(`https://api.linkedin.com/v2/images/${thumbnail}`, config);
+          const image = imageResponse.data.downloadUrl;
+          post.content.article.imageUrl = image;
+        }
+
+        if (post.content?.media?.id) {
+          const thumbnail = post.content.media.id;
+
+          if(thumbnail.includes("urn:li:video")) {
+            const videoResponse = await axios.get(`https://api.linkedin.com/v2/videos/${thumbnail}`, config);
+            const video = videoResponse.data.downloadUrl;
+            post.content.media.videoUrl = video;
+          }
+          else if(thumbnail.includes("urn:li:image")) {
+            const imageResponse = await axios.get(`https://api.linkedin.com/v2/images/${thumbnail}`, config);
+            const image = imageResponse.data.downloadUrl;
+            post.content.media.imageUrl = image;
+          }
+        }
+
+        if(post.content?.multiImage?.images) {
+          const images = post.content.multiImage.images;
+          images.forEach(async (image: any) => {
+            const imageResponse = await axios.get(`https://api.linkedin.com/v2/images/${image.id}`, config);
+            const imageUrl = imageResponse.data.downloadUrl;
+            image.imageUrl = imageUrl;
+          });
+        }
+        
+          
+        return post;
+      }));
+  
+      res.status(200).json(postsWithImage);
+    } catch (error) {
+      res.status(500).json(error);
+      console.error(error);
+    }
   }
 
   
