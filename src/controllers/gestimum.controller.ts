a54194b0a4fe87613989f1e6373c218586c8c0b4
Thomas Jankowski <thomas.jankowski@gestimum.com>
2023-07-03 16:01:16 +0200

    repos

--- src/controllers/gestimum.controller.ts
+++ src/controllers/gestimum.controller.ts
@@ -2,18 +2,39 @@ import { Request, Response } from 'express';
 import {connectionGestimum, executeQuery} from '../database';
 
 import colors from 'colors';
+import axios from 'axios';
 require('dotenv').config();
 
 //listes des utilisateurs avec l'entreprise associé
+// export const getGestimumClients = async (req: Request, res: Response) => {
+//     try {
+//         const query = 'SELECT PCF_CODE, PCF_RS, PCF_EMAIL, PCF_RUE, PCF_CP, PCF_VILLE, PAY_CODE, PCF_TYPE FROM TIERS ORDER BY PCF_RS, PCF_TYPE OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY';
+//       const tiers = await executeQuery(query);
+//       res.status(200).json(tiers);
+//     } catch (error) {
+//       console.error('Error executing query:', error);
+//       res.status(500).send({ message: 'Server Error' });
+//     }
+// };
+
 export const getGestimumClients = async (req: Request, res: Response) => {
-    try {
-        const query = 'SELECT PCF_CODE, PCF_RS, PCF_EMAIL, PCF_RUE, PCF_CP, PCF_VILLE, PAY_CODE, PCF_TYPE FROM TIERS ORDER BY PCF_RS, PCF_TYPE OFFSET 0 ROWS FETCH NEXT 10 ROWS ONLY';
-      const tiers = await executeQuery(query);
-      res.status(200).json(tiers);
-    } catch (error) {
-      console.error('Error executing query:', error);
-      res.status(500).send({ message: 'Server Error' });
+
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': process.env.TOKEN_SECRET,
     }
+  };
+
+  try {
+      const tiers = await axios.get(`http://localhost:4000/api/v1/clients/getGestimumClients`, config);
+      const firsts = tiers.data.clients.slice(0, 100);
+    res.status(200).json(firsts);
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).send({ message: 'Server Error' });
+  }
 };
 
 
